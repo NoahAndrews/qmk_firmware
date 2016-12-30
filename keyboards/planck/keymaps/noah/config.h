ac47c15e54aee4eb273db99b8b876b2415135a48
NoahAndrews <noahandrews@users.noreply.github.com>
2016-12-30 13:57:42 -0500

    Tweak mousekey options

--- keyboards/planck/keymaps/noah/config.h
+++ keyboards/planck/keymaps/noah/config.h
@@ -5,3 +5,9 @@
 #include "../../config.h"
 
 #define BACKLIGHT_BREATHING
+
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_INTERVAL       40
+#define MOUSEKEY_MAX_SPEED		6
+
+#define MOUSEKEY_WHEEL_MAX_SPEED 1
